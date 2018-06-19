9613da27623874efd6a124ffcdf50e01d9c0fff2
hm2810 <hassan.mlk@hotmail.com>
2018-06-20 00:02:33 +1000

    x

--- GD_lin_mod2.py
+++ GD_lin_mod2.py
@@ -6,6 +6,23 @@ data = np.genfromtxt('data-GD_lin_Siraj.csv', delimiter=',')
 learning_rate = 0.0001
 iter = 1000
 
+# =============================================================================
+# Loss function is Error**2, which is y_actual-y_predicted
+#  Now for gradient descent, we need to find derivative of Loss function wrt b0 and b1
+# For b0, from chain rule:
+#  d(Error**2)/d(b0) = d(Error**2)/d(Error) * d(Error)/d(b0)
+#  = 2 * Error * d(Error)/d(b0)
+#  = 2 * (y_actual - b0+b1*x) * d(y_actual - (b0+b1*x))/b0
+#  = 2 * (y_actual - b0+b1*x) * (0 - 1 - 0)
+#  = -2 * (y_actual - y_predicted)
+# Similarly, for b1, d(Error**2)/d(b1) = 2 * (y_actual - b0+b1*x) * (0 - 0 - x)
+#  = -2 * (y_actual - y_predicted) * x
+# Now, the b1 will be changed as, b1_new = b1_old - (deriv_b1 * learning rate)
+# Note that we are subtracting the derivative because derivative is negative,
+# however, if we have defined error as (y_predicted - y_actual) then derivative
+# would have been +ve so we had to add the derivative in above eq. instead of subtracting. 
+# 
+# =============================================================================
 
 def eq(x, b0, b1):
     #Define the equation
@@ -14,6 +31,7 @@ def eq(x, b0, b1):
 def calculate_error(b0,b1,data):
     #Initialize error
     SSE = 0
+    #Iteration over data list
     for i in range(0,len(data),1):
         x = data[i,0] #1st column in data is x
         y = data[i,1] #2nd column in data is y
@@ -43,10 +61,10 @@ def GDrun():
         for i in range(0,len(data),1):
             x = data[i,0] #1st column in data is x
             y = data[i,1] #2nd column in data is y
-            b0_deriv += -(2/N) * (y - ((b1 * x) + b0))
-            b1_deriv += -(2/N) * x * (y - ((b1 * x) + b0))
-        b0 = b0 - (learning_rate * b0_deriv)
-        b1 = b1 - (learning_rate * b1_deriv)
+            b0_deriv += -(2/N) * (y - eq(x,b0,b1)) #Explained above
+            b1_deriv += -(2/N) * x * (y - eq(x,b0,b1)) #Explained above
+        b0 = b0 - (b0_deriv * learning_rate) #Explained above
+        b1 = b1 - (b1_deriv * learning_rate) #Explained above
         #Print results
         print('After {0} iterations b0 = {1}, b1 = {2}, MSE = {3}'.format(i, b0, b1, calculate_error(b0, b1, data)))
     
